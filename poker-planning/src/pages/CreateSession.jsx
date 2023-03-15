3791de194f65d4159a0fc2626a55e1d056cb21d9
binaya-batas <batas.binaya@gmail.com>
2023-03-15 20:32:06 +0545

    Removed add user stories from create session.

--- poker-planning/src/pages/CreateSession.jsx
+++ poker-planning/src/pages/CreateSession.jsx
@@ -6,46 +6,21 @@ import ButtonAction from "../components/ButtonAction";
 import InputFieldWithBorder from "../components/InputFieldWithBorder";
 import InputField from '../components/InputField';
 
+import useSession from "../hooks/useSession";
+
 const CreateSession = () => {
     const [sessionName, setSessionName] = useState('')
-    const [inputFields, setInputFields] = useState([
-        {
-            storyPoint: "",
-            storyDescription: "",
-        },
-    ]);
 
-    const handleAddFieldButton = (event) => {
-        event.preventDefault();
+    const onChangeSessionName = (event) => {
+        setSessionName(event.target.value);
+    }
 
-        let newField = {
-            storyPoint: "",
-            storyDescription: "",
-        };
-
-        setInputFields([...inputFields, newField]);
-    };
+    const { createSession } = useSession();
 
     const handleCreateSession = (event) => {
         event.preventDefault();
-        console.log(inputFields, sessionName);
-    };
 
-    const onChangeInput = (index, event) => {
-        let data = [...inputFields];
-        data[index][event.target.name] = event.target.value;
-        setInputFields(data);
-    };
-
-    const onChangeSessionName = (e) => {
-        setSessionName(e.target.value);
-    }
-
-    const handleRemoveButton = (index, event) => {
-        event.preventDefault();
-        let data = [...inputFields];
-        data.splice(index, 1)
-        setInputFields(data)
+        createSession(sessionName);
     }
 
     return (
@@ -62,38 +37,8 @@ const CreateSession = () => {
                         onChange={onChangeSessionName}
                     />
 
-                    <div className="session__form__storypoint">
-                        <div className="session__form__storypoint__heading">Stories</div>
-                        {inputFields.map((input, index) => (
-                            <div
-                                key={`${index}${input}`}
-                                className="session__form__storypoint__inputfields"
-                            >
-                                <InputFieldWithBorder
-                                    type="text"
-                                    placeholder="Story Point"
-                                    name="storyPoint"
-                                    value={input.storyPoint}
-                                    onChange={(event) => onChangeInput(index, event)}
-                                />
-
-                                <InputFieldWithBorder
-                                    type="text"
-                                    placeholder="Story Description"
-                                    name="storyDescription"
-                                    value={input.storyDescription}
-                                    onChange={(event) => onChangeInput(index, event)}
-                                />
-
-                                <button onClick={() => handleRemoveButton(index, event)}>-</button>
-                            </div>
-                        ))}
-                        <div className="session-form__storypoint__add">
-                            <ButtonAction text="+" onClick={handleAddFieldButton} />
-                        </div>
-                        <div className="session__form__submit">
-                            <Button text="Create Session" onClick={handleCreateSession} />
-                        </div>
+                    <div className="session__form__submit">
+                        <Button text="Create Session" onClick={handleCreateSession} />
                     </div>
                 </form>
             </div>

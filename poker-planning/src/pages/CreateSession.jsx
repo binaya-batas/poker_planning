18c97604a23056ca0a8b8a7eb766fd65f6b25d13
binaya-batas <batas.binaya@gmail.com>
2023-03-19 16:43:58 +0545

    protected routes for logged in users only.

--- poker-planning/src/pages/CreateSession.jsx
+++ poker-planning/src/pages/CreateSession.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 import Button from "../components/Button";
 import ButtonAction from "../components/ButtonAction";
 import InputFieldWithBorder from "../components/InputFieldWithBorder";
 import InputField from '../components/InputField';
 
 import useSession from "../hooks/useSession";
+import { Navigate } from "react-router-dom";
 
 const CreateSession = () => {
     const [sessionName, setSessionName] = useState('')
     const [sessionLink, setSessionLink] = useState('')
+    const loggedIn = JSON.parse(sessionStorage.getItem("loggedIn"));
 
     const { createSession, joinSession } = useSession();
     
@@ -23,48 +28,83 @@ const CreateSession = () => {
 
     const handleCreateSession = (event) => {
         event.preventDefault();
+
+        if (!sessionName.trim().length) {
+            toast.warning("You must provide a sessin name before creating a session.", {
+              position: "top-right",
+              autoClose: 1000,
+              hideProgressBar: true,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "dark",
+            });
+            return;
+          }
         createSession(sessionName);
     }
 
     const handleCreateSessionLink = (event) => {
         event.preventDefault();
 
+        if (!sessionLink.trim().length) {
+            toast.warning("You must provide a link before joining a session.", {
+              position: "top-right",
+              autoClose: 1000,
+              hideProgressBar: true,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "dark",
+            });
+            return;
+          }
+
         let link= sessionLink.split("/");
         joinSession(link[4]);
     }
 
     return (
-        <div className="container">
-            <div className="session">
-                <div className="session__heading">Create session</div>
+        <>
+        {
+            loggedIn ?
+            <div className="container">
+                <div className="session">
+                    <div className="session__heading">Create session</div>
 
-                <form action="" className="session__form">
-                    <InputFieldWithBorder
-                        type="text"
-                        placeholder="Session Name"
-                        name="sessionName"
-                        value={sessionName}
-                        onChange={onChangeSessionName}
-                    />
+                    <form action="" className="session__form">
+                        <InputFieldWithBorder
+                            type="text"
+                            placeholder="Session Name"
+                            name="sessionName"
+                            value={sessionName}
+                            onChange={onChangeSessionName}
+                        />
 
-                    <div className="session__form__submit">
-                        <Button text="Create Session" onClick={handleCreateSession} />
-                    </div>
+                        <div className="session__form__submit">
+                            <Button text="Create Session" onClick={handleCreateSession} />
+                        </div>
 
-                    <InputFieldWithBorder
-                        type="text"
-                        placeholder="Paste your invitation link here"
-                        name="sessionLink"
-                        value={sessionLink}
-                        onChange={onChangeSessionLink}
-                    />
+                        <InputFieldWithBorder
+                            type="text"
+                            placeholder="Paste your invitation link here"
+                            name="sessionLink"
+                            value={sessionLink}
+                            onChange={onChangeSessionLink}
+                        />
 
-                    <div className="session__form__submit">
-                        <Button text="Join Session" onClick={handleCreateSessionLink} />
-                    </div>
-                </form>
+                        <div className="session__form__submit">
+                            <Button text="Join Session" onClick={handleCreateSessionLink} />
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+            :
+            <Navigate to='/login' />
+        }
+        </>
     );
 };
 
--- poker-planning/src/pages/Dashboard.jsx
+++ poker-planning/src/pages/Dashboard.jsx
@@ -1,37 +1,106 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Navigate, useParams } from 'react-router-dom'
 
 import Button from '../components/Button'
 import AddStoryPoints from '../components/AddStoryPoints'
 import MemberList from '../components/MemberList'
 import StoryPointsList from '../components/StoryPointsList'
 
+import useStoryPoint from '../hooks/useStoryPoint';
 import useSession from '../hooks/useSession';
+import StoriesList from '../components/StoriesList'
+import Navbar from '../components/Navbar'
 
 const Dashboard = () => {
-    // const [showAddStories, setShowAddStories] = useState(false);
-
-    const { getSessionMembers, members } = useSession();
+    let user = JSON.parse(sessionStorage.getItem("user"));
+    const { addStoryPoint } = useStoryPoint();
+    const { getSessionMembers, getSessionUserStories, getStoryByActiveStatus, members, stories, currentStory } = useSession();
     const { id } = useParams();
-
+    const loggedIn = JSON.parse(sessionStorage.getItem("loggedIn"));
+    
+    const [storyPoint, setStoryPoint] = useState({
+        storyId: '',
+        userId: '',
+        storyPoint: ''
+      });
+    
     useEffect(() => {
         getSessionMembers(id);
-    }, [members])
+
+        setInterval(() => {
+            getSessionMembers(id);
+        }, 10000)
+    }, [])
+
+    console.log(members);
+
+    useEffect(() => {
+        getSessionUserStories(id);
+
+        setInterval(() => {
+            getSessionUserStories(id);
+        }, 10000)
+    }, [])
+
+    useEffect(() => {
+        getStoryByActiveStatus(id);
+
+        setInterval(() => {
+            getStoryByActiveStatus(id);
+        }, 10000)
+    }, [])
+    
+    const handleVoteClick = (event) => {
+        event.preventDefault();
+        addStoryPoint(storyPoint);
+    }
+
+    //since moderator is always at 0 index of members list.
+    let moderator = members[0];
 
     return (
-        <div className="container">
-            <div className='dashboard'>
-                <MemberList members={members} />
-                <div className="dashboard__vote">
-                    <div className="dashboard__vote__title">Click to vote</div>
-                    <StoryPointsList />
+        <>
+        {
+                loggedIn ?
+                <>
+                <Navbar sessionId={id} />
+                <div className="container">
+                    <div className='dashboard'>
+                        {/* <StoriesList stories={stories} deleteStatus={user.id === moderator?.id ? true : false} currentStory={setCurrentStory} /> */}
+                        <StoriesList stories={stories} deleteStatus={user.id === moderator?.id ? true : false} sessionId={id} />
+                        <section className="dashboard__vote">
+                            <div className="dashboard__vote__title">Click to vote</div>
+                                <div className="dashboard__userstory">
+                                    <div className="dashboard__userstory__title">{currentStory?.story_title}</div> 
+                                    <div className="dashboard__userstory__description">{currentStory?.story_description}</div>
+                                </div>
+                            <StoryPointsList currentStory={currentStory} user={user} setStoryPoint={setStoryPoint} />
+                            <Button text="Vote" onClick={handleVoteClick}>Vote</Button>
+                        </section>
+                        <MemberList members={members} />
+                        {/* {
+                            user.id === moderator?.id &&
+                            <Button text="Reveal" />
+                        } */}
+                        {
+                            // Displays add storypoints form only if the user.id is equal to moderator id
+                            user.id === moderator?.id &&
+                            <section className="dashboard__storypoints">
+                                <div className="dashboard__storypoints__title">Add Storypoints</div>
+                                <AddStoryPoints id={id} />
+                            </section>
+                        }
+                        {
+                            user.id === moderator?.id &&
+                            <button>Reveal</button>
+                        }
+                    </div>
                 </div>
-                <div className="dashboard__storypoints">
-                    <div className="dashboard__storypoints__title">Add Storypoints</div>
-                    <AddStoryPoints />
-                </div>
-            </div>
-        </div>
+                </>
+                :
+                <Navigate to="/login" />
+        }
+        </>
     )
 }
 
